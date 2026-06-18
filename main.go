ee237b15778bd18004d1cec3f4288a172ef6b772
Lars Kellogg-Stedman <lars@oddbit.com>
2026-06-18 09:36:18 -0400

    fix: Add missing GPIO15

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var gpioPins = []machine.Pin{
 	machine.GPIO0, machine.GPIO1, machine.GPIO2, machine.GPIO3,
 	machine.GPIO4, machine.GPIO5, machine.GPIO6, machine.GPIO7,
 	machine.GPIO8, machine.GPIO9, machine.GPIO10, machine.GPIO11,
-	machine.GPIO12, machine.GPIO13, machine.GPIO14,
+	machine.GPIO12, machine.GPIO13, machine.GPIO14, machine.GPIO15,
 	machine.GPIO17, machine.GPIO18, machine.GPIO19,
 	machine.GPIO20, machine.GPIO21, machine.GPIO22, machine.GPIO23,
 	machine.GPIO26, machine.GPIO27, machine.GPIO28, machine.GPIO29,
